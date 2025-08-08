307445971e74cd18d9b489462d995978f3315f67
webpiratt <213060745+webpiratt@users.noreply.github.com>
2025-08-09 01:08:11 +0300

    recipe schema

--- internal/dca/spec.go
+++ internal/dca/spec.go
@@ -9,12 +9,14 @@ import (
 )
 
 const (
-	startDate = "startDate"
+	endDate = "endDate"
 )
 
 const (
 	frequency = "frequency"
 
+	minutely = "minutely"
+	hourly   = "hourly"
 	daily    = "daily"
 	weekly   = "weekly"
 	biWeekly = "bi-weekly"
@@ -24,8 +26,82 @@ const (
 type Spec struct{}
 
 func (s *Spec) GetRecipeSpecification() (*rtypes.RecipeSchema, error) {
+	cfg, err := plugin.RecipeConfiguration(map[string]any{
+		"type": "object",
+		"properties": map[string]any{
+			endDate: map[string]any{
+				"type":   "string",
+				"format": "date-time",
+			},
+			frequency: map[string]any{
+				"type": "string",
+				"enum": []any{
+					minutely,
+					hourly,
+					daily,
+					weekly,
+					biWeekly,
+					monthly,
+				},
+			},
+		},
+		"required": []any{
+			frequency,
+		},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to build pb recipe config: %w", err)
+	}
+
 	return &rtypes.RecipeSchema{
-		// TODO
+		Version:       1, // Schema version
+		PluginId:      string(types.PluginVultisigDCA_0000),
+		PluginName:    "DCA",
+		PluginVersion: 1,
+		SupportedResources: []*rtypes.ResourcePattern{
+			{
+				ResourcePath: &rtypes.ResourcePath{
+					ChainId:    "ethereum",
+					ProtocolId: "uniswapV2_router",
+					FunctionId: "swapExactTokensForTokens",
+					Full:       "ethereum.uniswapV2_router.swapExactTokensForTokens",
+				},
+				Target: rtypes.TargetType_TARGET_TYPE_ADDRESS,
+				ParameterCapabilities: []*rtypes.ParameterConstraintCapability{
+					{
+						ParameterName:  "amountIn",
+						SupportedTypes: rtypes.ConstraintType_CONSTRAINT_TYPE_FIXED,
+						Required:       true,
+					},
+					{
+						ParameterName:  "amountOutMin",
+						SupportedTypes: rtypes.ConstraintType_CONSTRAINT_TYPE_MIN,
+						Required:       true,
+					},
+					{
+						ParameterName:  "path",
+						SupportedTypes: rtypes.ConstraintType_CONSTRAINT_TYPE_FIXED,
+						Required:       true,
+					},
+					{
+						ParameterName:  "to",
+						SupportedTypes: rtypes.ConstraintType_CONSTRAINT_TYPE_FIXED,
+						Required:       true,
+					},
+					{
+						ParameterName:  "deadline",
+						SupportedTypes: rtypes.ConstraintType_CONSTRAINT_TYPE_MIN,
+						Required:       true,
+					},
+				},
+				Required: true,
+			},
+		},
+		Configuration: cfg,
+		Requirements: &rtypes.PluginRequirements{
+			MinVultisigVersion: 1,
+			SupportedChains:    []string{"ethereum"},
+		},
 	}, nil
 }
 
