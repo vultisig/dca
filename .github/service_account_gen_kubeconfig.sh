823ee4f2c478b640dd25fdace4a731515763100b
webpiratt <213060745+webpiratt@users.noreply.github.com>
2025-12-12 00:17:43 +0300

    deploy

--- .github/service_account_gen_kubeconfig.sh
+++ .github/service_account_gen_kubeconfig.sh
@@ -1,6 +1,7 @@
 server=https://0.0.0.0:6443
 name=github-actions-token
 
+
 ca=$(kubectl get secret/$name -o jsonpath='{.data.ca\.crt}')
 token=$(kubectl get secret/$name -o jsonpath='{.data.token}' | base64 --decode)
 namespace=$(kubectl get secret/$name -o jsonpath='{.data.namespace}' | base64 --decode)
