5597f5b8685cb191b776236e8be39bfee13f82b7
AvranWong <avranwong@gmail.com>
2026-01-03 22:19:19 +0800

    use default interval in scheduler

--- internal/recurring/scheduler.go
+++ internal/recurring/scheduler.go
@@ -3,12 +3,29 @@ package recurring
 import (
 	"context"
 	"errors"
+	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/vultisig/verifier/plugin/scheduler"
 	"github.com/vultisig/verifier/types"
 )
+
+// parseDateTime parses a date string that can be either RFC3339 format or Unix milliseconds
+func parseDateTime(dateStr string) (time.Time, error) {
+	// Try RFC3339 first
+	if t, err := time.Parse(time.RFC3339, dateStr); err == nil {
+		return t, nil
+	}
+
+	// Try Unix milliseconds (e.g., "1765464900000")
+	if ms, err := strconv.ParseInt(dateStr, 10, 64); err == nil {
+		return time.UnixMilli(ms), nil
+	}
+
+	return time.Time{}, fmt.Errorf("invalid date format: %s (expected RFC3339 or Unix milliseconds)", dateStr)
+}
 
 type SchedulerService struct {
 	repo scheduler.Storage
