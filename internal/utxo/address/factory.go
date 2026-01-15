11871df9aedeca654000090abb77771e7022dec8
AvranWong <avranwong@gmail.com>
2026-01-15 15:55:55 +0800

    fix code rabbit

--- internal/utxo/address/factory.go
+++ internal/utxo/address/factory.go
@@ -2,6 +2,7 @@ package address
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/vultisig/vultisig-go/common"
 )
@@ -23,6 +24,8 @@ func NewFromString(chain common.Chain, addrStr string) (UTXOAddress, error) {
 }
 
 // NewFromPubKeyHash creates a UTXOAddress from a pubkey hash based on chain.
+// Note: The segwit parameter is ignored for BCH and DOGE as these chains do not support SegWit.
+// BCH uses CashAddr format (P2PKH only), and DOGE uses legacy P2PKH addresses.
 func NewFromPubKeyHash(chain common.Chain, pubKeyHash []byte, segwit bool) (UTXOAddress, error) {
 	switch chain {
 	case common.Bitcoin:
@@ -30,8 +33,14 @@ func NewFromPubKeyHash(chain common.Chain, pubKeyHash []byte, segwit bool) (UTXO
 	case common.Litecoin:
 		return NewLTCAddressFromPubKeyHash(pubKeyHash, segwit)
 	case common.BitcoinCash:
+		if segwit {
+			log.Printf("[UTXO-ADDRESS] Warning: segwit=true ignored for %s - BCH does not support SegWit, using CashAddr P2PKH", chain.String())
+		}
 		return NewBCHAddressFromPubKeyHash(pubKeyHash)
 	case common.Dogecoin:
+		if segwit {
+			log.Printf("[UTXO-ADDRESS] Warning: segwit=true ignored for %s - DOGE does not support SegWit, using legacy P2PKH", chain.String())
+		}
 		return NewDOGEAddressFromPubKeyHash(pubKeyHash)
 	default:
 		return nil, fmt.Errorf("unsupported UTXO chain: %s", chain)
